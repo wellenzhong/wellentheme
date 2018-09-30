0c2d4223692bdf5c2e926cc12529635585a80c3f
yz-zhongweiren <zhongweiren@yinzi.com>
2018-09-30 17:35:59 +0800

    响应式

--- app/javascript/theme.js
+++ app/javascript/theme.js
@@ -2,3 +2,11 @@
 var mySwiper = new Swiper('.swiper-container', {
     autoplay: 5000,//可选选项，自动滑动
 })
+jQuery(".mobile-menu").click(function () {
+    jQuery("body").css("overflow", "hidden")
+    jQuery(".mobile-screen").addClass('screen-active')
+})
+jQuery(".mobile-back").click(function () {
+    jQuery("body").css("overflow", "auto");
+    jQuery(".mobile-screen").removeClass('screen-active')
+})
